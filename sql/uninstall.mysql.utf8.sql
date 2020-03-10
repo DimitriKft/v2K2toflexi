21c35dcce20d2eb00b5ea1ab9ad361008341d5fb
klopfstein <dimitri.kft@gmail.com>
2020-03-10 11:32:58 +0100

    update uninstall sql

--- sql/uninstall.mysql.utf8.sql
+++ sql/uninstall.mysql.utf8.sql
@@ -1,2 +1,7 @@
 DROP TABLE IF EXISTS `#__k2toflexi_log`;
 DROP TABLE IF EXISTS `#__k2toflexi_tags`;
+DROP TABLE IF EXISTS `#__k2toflexi_items`;
+DROP TABLE IF EXISTS `#__k2toflexi_attachments`;
+DROP TABLE IF EXISTS `#__k2toflexi_categories`;
+DROP TABLE IF EXISTS `#__k2toflexi_extra_fields_groups`;
+DROP TABLE IF EXISTS `#__k2toflexi_extra_fields`;
